ad10211111966ad99fe1f6cf1886cf1afe3cad81
di.doche <dimitry.doche2@isae-supaero.com>
2026-01-02 23:40:46 +0100

    Add lib.rs and rename crate to astro_pi_plate_solving

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+mod coordinate;
+mod parse_catalog;
+mod printing;
+mod solver;
+mod star_quads;
+mod platesolve;
+
+#[cfg(test)]
+mod tests;
+
+// Re-export public API
+pub use coordinate::{Arcdegrees, CoordinateEquatorial, RaHoursMinutesSeconds, Star};
+pub use platesolve::{convert_cr3_to_dng, solve_plate, solve_plate_with_options, PlateSolvingResult, ImageAnalysisResult, TransformCoefficients};
